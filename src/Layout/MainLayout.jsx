7ec174711156d840a2fd9ffbc5b0b73aabbe22ea
Sagar <ishowsagar777@gmail.com>
2025-11-24 18:05:34 +0530

    added mre posts for feed

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -6,28 +6,28 @@ import { useEffect, useState, createContext, useContext } from "react";
 const postDataContext = createContext();
 
 export default function MainLayout() {
-    const [postData, setPostData] = useState([]);
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const response = await fetch(
-            "https://jsonplaceholder.typicode.com/posts"
-          );
-          if (!response.ok) {
-            throw new Error("error fetching data");
-          }
-          const data = await response.json();
-          setPostData(data?.slice(0, 5));
-        } catch (err) {
-          console.error("caught error while fetching data -", err);
+  const [postData, setPostData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        if (!response.ok) {
+          throw new Error("error fetching data");
         }
+        const data = await response.json();
+        setPostData(data?.slice(0, 45));
+      } catch (err) {
+        console.error("caught error while fetching data -", err);
       }
-      fetchData();
-    }, []);
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
-      <postDataContext.Provider value={{postData, setPostData}}>
+      <postDataContext.Provider value={{ postData, setPostData }}>
         <section className="Site_wrapper">
           <Header />
 
